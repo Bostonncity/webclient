bb7adf4a0e4529dbc0fba168bbdb1f178bffde56
joaoevangelista <evangelistajoaop@gmail.com>
2015-12-12 02:19:57 -0200

    index banners

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -5,8 +5,13 @@ export default Ember.Controller.extend({
   actions: {
     search() {
       console.log(this);
-      let query =  this.get('searchInput');
+      let query = this.get('searchInput');
       console.log(`Searching for ${query}`);
+    },
+    scrollDown() {
+      Ember.$('html,body').animate({
+        scrollTop: Ember.$('#marketing').offset().top
+      }, 2000);
     }
   }
 });
