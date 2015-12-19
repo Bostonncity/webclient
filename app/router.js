f4f9d220a5a3cf3053e9ca2dfc5b4d9e1dc6f1f6
joaoevangelista <evangelistajoaop@gmail.com>
2015-12-19 17:14:31 -0200

    New labels and center text

--- app/router.js
+++ app/router.js
@@ -10,7 +10,7 @@ Router.map(function () {
   this.route('sign_in');
   this.route('login');
   this.route('places', function () {
-    this.route('single', {path: 'places/{id}'});
+    this.route('single', {path: ':id'});
   });
 });
 
--- app/routes/places/single.js
+++ app/routes/places/single.js
@@ -1,4 +1,24 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model: function(){
+    return {
+      "name":"Cliffs of dover",
+      "description":"Beautiful cliff, where you can see the city below on sunny days",
+      "activities":[
+        {
+          "name":"photography"
+        },
+        {
+          "name":"jumping"
+        },
+        {
+          "name":"climbing"
+        },
+        {
+          "name":"yoga"
+        }
+      ]
+    }
+  }
 });
