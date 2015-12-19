f8ecebd173cfc27551df53cb36215212f95be5f1
joaoevangelista <evangelistajoaop@gmail.com>
2015-12-19 17:58:52 -0200

    Dropdown options as main menu on nav bar

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -24,7 +24,6 @@ module.exports = function (defaults) {
   app.import("bower_components/bootstrap/dist/css/bootstrap.min.css");
   app.import("bower_components/bootstrap/dist/js/bootstrap.min.js");
   app.import("bower_components/flat-ui/dist/css/flat-ui.min.css");
-  app.import("bower_components/flat-ui/dist/js/flat-ui.min.js");
   app.import("bower_components/font-awesome/css/font-awesome.min.css");
   var flatUiFonts = new Funnel('bower_components/flat-ui/dist/fonts', {
     destDir: 'fonts'
